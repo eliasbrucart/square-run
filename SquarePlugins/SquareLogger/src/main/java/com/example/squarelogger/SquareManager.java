a56fcff76170c609056e98725fb097a68fd003a2
Elias <eliasbrucart@hardgames.com.ar>
2021-10-22 12:24:35 -0300

    Added SquareLogger plugin for Android

---
+++ SquarePlugins/SquareLogger/src/main/java/com/example/squarelogger/SquareManager.java
@@ -0,0 +1,15 @@
+package com.example.squarelogger;
+
+import android.util.Log;
+
+public class SquareManager {
+    public static final SquareManager _instance = new SquareManager();
+
+    public static SquareManager GetInstance(){
+        return _instance;
+    }
+
+    public void SendLog(String msg){
+        Log.d("SM=>", msg);
+    }
+}
