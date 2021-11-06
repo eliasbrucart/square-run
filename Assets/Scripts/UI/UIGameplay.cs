680c498bae124db1f4d8c312ff47ea12b93fc99e
Elias <eliasbrucart@hardgames.com.ar>
2021-11-06 02:06:49 -0300

    Added wait timer to UI

--- Assets/Scripts/UI/UIGameplay.cs
+++ Assets/Scripts/UI/UIGameplay.cs
@@ -5,6 +5,7 @@ public class UIGameplay : MonoBehaviour
 {
     public TMP_Text timerText;
     public TMP_Text scoreText;
+    public TMP_Text waitTimer;
     private GameManager gm;
     void Start()
     {
@@ -15,5 +16,13 @@ public class UIGameplay : MonoBehaviour
     {
         timerText.text = "" + gm.timer.ToString("F0");
         scoreText.text = "" + gm.score.ToString("F0");
+        waitTimer.text = "" + gm.timerWaitTime.ToString("F0");
+        DisbleWaitTimer();
+    }
+
+    private void DisbleWaitTimer()
+    {
+        if (gm.timerWaitTime >= 3.0f)
+            waitTimer.gameObject.SetActive(false);
     }
 }
