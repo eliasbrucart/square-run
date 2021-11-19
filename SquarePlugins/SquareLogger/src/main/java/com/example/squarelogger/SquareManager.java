f2845fec993cd404a7ee4f5b9a71ee53702030c7
Elias <eliasbrucart@hardgames.com.ar>
2021-11-19 00:49:56 -0300

    Android plugin: added save score function

--- SquarePlugins/SquareLogger/src/main/java/com/example/squarelogger/SquareManager.java
+++ SquarePlugins/SquareLogger/src/main/java/com/example/squarelogger/SquareManager.java
@@ -1,9 +1,16 @@
 package com.example.squarelogger;
 
+import java.io.File;
+import android.app.Activity;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.util.Log;
 
 public class SquareManager {
     public static final SquareManager _instance = new SquareManager();
+    public static Activity activity;
 
     public static SquareManager GetInstance(){
         Log.d("SquareLogger->", "Esta funcionando GetInstance");
@@ -12,5 +19,28 @@ public class SquareManager {
 
     public void SendLog(String msg){
         Log.d("SM=>", msg);
+    }
+
+    public void SaveLogs(){
+        File file;
+    }
+
+    public void SaveScore(int score){
+        File path = activity.getFilesDir();
+        File file = new File(path, "score.txt");
+
+        try{
+            FileOutputStream stream = new FileOutputStream(file);
+            try
+            {
+                stream.write(Integer.toString(score).getBytes());
+            }
+            finally {
+                stream.close();
+            }
+        }
+        catch(IOException e){
+            Log.e("Exception", "Error: the file could not be recorded" + e.toString());
+        }
     }
 }
