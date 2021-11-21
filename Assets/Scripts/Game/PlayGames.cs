1df0bef112744ab04932776ae978b8971e6f3796
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-11-21 18:54:52 -0300

    Established play game services

---
+++ Assets/Scripts/Game/PlayGames.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
+
+public class PlayGames : MonoBehaviour
+{
+    public TMP_Text playerScore;
+    string leaderboardID = "CgkIqfmmyPsTEAIQAg";
+    string achievementID = "CgkIqfmmyPsTEAIQAQ";
+    public static PlayGamesPlatform platform;
+
+    void Start()
+    {
+        if (platform == null)
+        {
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+            PlayGamesPlatform.InitializeInstance(config);
+            PlayGamesPlatform.DebugLogEnabled = true;
+            platform = PlayGamesPlatform.Activate();
+        }
+
+        Social.Active.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                Debug.Log("Logged in successfully");
+            }
+            else
+            {
+                Debug.Log("Login Failed");
+            }
+        });
+        UnlockAchievement();
+    }
+
+    public void AddScoreToLeaderboard()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            Social.ReportScore(int.Parse(playerScore.text), leaderboardID, success => { });
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            platform.ShowLeaderboardUI();
+        }
+    }
+
+    public void ShowAchievements()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            platform.ShowAchievementsUI();
+        }
+    }
+
+    public void UnlockAchievement()
+    {
+        if (Social.Active.localUser.authenticated)
+        {
+            Social.ReportProgress(achievementID, 100f, success => { });
+        }
+    }
+}
