7f01abd808f7fce0621422c4c7172dc7ec12dd72
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-11-26 02:47:01 -0300

    Settings scene now works

---
+++ Assets/Scripts/Game/AMReference.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AMReference : MonoBehaviour
+{
+    private AudioManager AMInstance;
+    void Start()
+    {
+        AMInstance = FindObjectOfType<AudioManager>();
+    }
+
+    public void ChangeStateMusic()
+    {
+        if(AMInstance != null)
+            AMInstance.EnableMusic();
+    }
+
+    public void ChangeStateSFX()
+    {
+        if (AMInstance != null)
+            AMInstance.EnableSFX();
+    }
+}
--- Assets/Scripts/Game/AudioManager.cs
+++ Assets/Scripts/Game/AudioManager.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     static public AudioManager instanceAudioManager;
     static public AudioManager Instance { get { return instanceAudioManager; } }
+
+    public AudioSource bgMusic;
+    public AudioSource sfxSound;
+
+    //private bool state;
 
     private void Awake()
     {
@@ -17,11 +23,22 @@ public class AudioManager : MonoBehaviour
     }
     void Start()
     {
-        
+        bgMusic.volume = 0.3f;
+        sfxSound.volume = 0.5f;
     }
 
     void Update()
     {
         
+    }
+
+    public void EnableMusic()
+    {
+        bgMusic.mute = !bgMusic.mute;
+    }
+
+    public void EnableSFX()
+    {
+        sfxSound.mute = !sfxSound.mute;
     }
 }
