ce2e66401b71d3d4fd37c1fd36d8db12fec2cfa8
Elias Brucart <eliasbrucart@hardgames.com.ar>
2021-11-26 01:10:30 -0300

    Added AudioManager

---
+++ Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static public AudioManager instanceAudioManager;
+    static public AudioManager Instance { get { return instanceAudioManager; } }
+
+    private void Awake()
+    {
+        if (instanceAudioManager != this && instanceAudioManager != null)
+            Destroy(gameObject);
+        else
+            instanceAudioManager = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
