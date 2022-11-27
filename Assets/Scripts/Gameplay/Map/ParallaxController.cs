a2cfdaef6812c18be0f8994ad9a636026cd04d7c
eliasbrucart <eliasbrucart@hardgames.com.ar>
2022-11-26 22:14:42 -0300

    Added parallax to gameplay

---
+++ Assets/Scripts/Gameplay/Map/ParallaxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> backgrounds = new List<GameObject>();
+    [SerializeField] private float minY = 0;
+    [SerializeField] private float maxY = 0;
+
+    void Update()
+    {
+        for(int i = 0; i < backgrounds.Count; i++)
+        {
+            if (backgrounds[i].transform.position.y > maxY)
+                backgrounds[i].transform.position += Vector3.down * 2.0f * Time.deltaTime;
+            else
+                backgrounds[i].transform.position = new Vector3(0.0f, minY, 0.0f);
+        }
+    }
+}
